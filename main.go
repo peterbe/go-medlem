ba6275ae34772662a6570294b17e8d714c23cf9d
Peter Bengtsson <mail@peterbe.com>
2016-06-22 11:27:45 -0400

    wip

--- main.go
+++ main.go
@@ -17,20 +17,79 @@ func helloworld(ctx *iris.Context) {
 	ctx.Write("Hi %s\n", "world")
 }
 
+type UsersForm struct {
+	Emails []string `form:"email"`
+}
+
+type UsersJSON struct {
+	Emails []string `json:"email"`
+}
+
+// func logger(format string, a ...interface{}) {
+//     iris.Logger.Infof(format, a)
+// }
+
+func isStaff(ctx *iris.Context) {
+	jsonUsers := UsersJSON{}
+	var emails []string
+	jsonErr := ctx.ReadJSON(&jsonUsers)
+
+	if jsonErr != nil {
+		// XXX return a BadRequest if the attempt really was JSON
+		log.Println("Error when reading JSON body: " + jsonErr.Error())
+	} else {
+		// log.Println("Users", jsonUsers)
+		emails = jsonUsers.Emails
+	}
+	if jsonErr != nil || len(emails) == 0 {
+		// Read form form data or query string
+		formUsers := UsersForm{}
+		formErr := ctx.ReadForm(&formUsers)
+		if formErr != nil {
+			log.Println("Error when reading form: " + formErr.Error())
+		} else {
+			emails = formUsers.Emails
+		}
+	}
+
+	if len(emails) == 0 {
+		// ctx.Write("No emails supplied. See docs\n")
+		ctx.JSON(iris.StatusBadRequest, iris.Map{
+			"error": "No emails supplied. See docs",
+		})
+		ctx.SetStatusCode(iris.StatusBadRequest) // 400
+	} else {
+		// log.Println("Users", emails)
+		results := make(map[string]bool)
+		for _, email := range emails {
+			// log.Println("EMAIL:", email)
+			results[email] = false
+		}
+
+		// ctx.JSON(iris.StatusOK, iris.Map{
+		// 	"emails":  emails,
+		// })
+		ctx.JSON(iris.StatusOK, results)
+	}
+
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-
 	debug := os.Getenv("DEBUG") == "true"
 	if debug {
-		log.Println("Running in debug mode")
+		// log.Println("Running in debug mode")
+		iris.Logger.Infof("Running in debug mode")
 		iris.Config.Render.Template.IsDevelopment = true
 	}
 
 	api := iris.New()
 	api.Get("/helloworld", helloworld)
+	api.Get("/staff", isStaff)
+	api.Post("/staff", isStaff)
 	api.Get("/", index)
 	api.Listen("0.0.0.0:" + port)
 }
