3401a9d4cd474f2ffa441f5f17580ac419cd1337
Peter Bengtsson <mail@peterbe.com>
2016-06-22 10:30:01 -0400

    debug mode

--- main.go
+++ main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-    "log"
-    "fmt"
-    "os"
-    "github.com/kataras/iris"
-    )
+	"github.com/kataras/iris"
+	"log"
+	"os"
+)
 
-func index(ctx *iris.Context){
-   // ctx.Render("index.html", struct { Name string }{ Name: "iris" })
-   // maybe ctx.Render("index.html", nil)
-   var context struct{}
-   ctx.Render("index.html", context)
+func index(ctx *iris.Context) {
+	// ctx.Render("index.html", struct { Name string }{ Name: "iris" })
+	// maybe ctx.Render("index.html", nil)
+	var context struct{}
+	ctx.Render("index.html", context)
 }
 
-func helloworld(ctx *iris.Context){
-   ctx.Write("Hi %s\n", "world")
+func helloworld(ctx *iris.Context) {
+	ctx.Write("Hi %s\n", "world")
 }
-
 
 func main() {
-    port := os.Getenv("PORT")
-
+	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
-	} else {
-        fmt.Sprintf("PORT=%s\n", port)
-    }
+	}
 
-    // iris.Config.Render.Template.IsDevelopment = true
+	debug := os.Getenv("DEBUG") == "true"
+	if debug {
+		log.Println("Running in debug mode")
+		iris.Config.Render.Template.IsDevelopment = true
+	}
 
-    api := iris.New()
-    api.Get("/helloworld", helloworld)
-    api.Get("/", index)
-    api.Listen("0.0.0.0:" + port)
+	api := iris.New()
+	api.Get("/helloworld", helloworld)
+	api.Get("/", index)
+	api.Listen("0.0.0.0:" + port)
 }
