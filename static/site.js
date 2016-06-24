d21adf2f142174751d5d47f6591778692a4c4e56
Peter Bengtsson <mail@peterbe.com>
2016-06-24 10:41:55 -0400

    pretty home page

--- static/site.js
+++ static/site.js
@@ -1,4 +1,8 @@
 $(() => {
+  $('.baseurl').text(
+    document.location.protocol + '//' + document.location.host
+  );
+
   $('form').on('submit', (event) => {
     event.preventDefault()
     let emails = []
@@ -15,11 +19,12 @@ $(() => {
           $('<td>').text(e)
         )
         .append(
-          $('<td>').text('...')
+          $('<td>')
+          .append($('<span class="tag">hmm</span>'))
         )
         .appendTo(container)
     })
-    $('#results').show();
+    $('#results').show()
     if (emails.length) {
       $.ajax({
           url: '/staff',
@@ -33,9 +38,14 @@ $(() => {
           $('#error').hide()
           $('tr', container).each((i, row) => {
             let email = $('td:first-child', row).text();
-            $('td:last-child', row).text(r[email])
+            // $('td:last-child', row).text(r[email])
+            $('td:last-child span', row).remove()
             if (r[email]) {
-              $('td:last-child', row).css('font-weight', 'bold')
+              $('td:last-child', row)
+              .append($('<span class="tag is-success">Yes!</span>'))
+            } else {
+              $('td:last-child', row)
+              .append($('<span class="tag is-danger">Nope</span>'))
             }
           })
         })
@@ -43,6 +53,7 @@ $(() => {
           // XXX check if the status code was 400
           $('#error pre')
             .text(JSON.stringify(JSON.parse(err.responseText), undefined, 2))
+          $('#results').hide()
           $('#error').show()
           console.error(err);
           // alert('Failed')
